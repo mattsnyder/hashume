b9b85125d9d7ea9e82837131ce0d55becbd78295
Matt Snyder <snyder2112@me.com>
2013-12-19 14:32:23 -0500

    Use keroku config for prod db

--- config/initializers/curator.rb
+++ config/initializers/curator.rb
@@ -2,5 +2,15 @@ Curator.configure(:mongo) do |config|
   config.database = "hashume"
   config.environment = Rails.env
   config.migrations_path = Rails.root.join("db/migrate")
-  config.mongo_config_file = Rails.root.join("config/mongo.yml")
+
+  if Rails.env == 'production'
+    db = URI.parse(ENV['MONGOHQ_URL'])
+    db_name = db.path.gsub(/^\//, '')
+    connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+    connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+    config.client = connection
+  else
+    config.client = Mongo::Connection.new
+  end
+
 end
