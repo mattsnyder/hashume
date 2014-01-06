5cdf72307baff6a76deac0e79caf14a3a95952f1
Matt Snyder <snyder2112@me.com>
2014-01-06 09:47:59 -0500

    Use ENV vars for mongo as needed

--- config/initializers/curator.rb
+++ config/initializers/curator.rb
@@ -1,16 +1,16 @@
 Curator.configure(:mongo) do |config|
-  config.database = "hashume"
   config.environment = Rails.env
   config.migrations_path = Rails.root.join("db/migrate")
+  config.database = "hashu"
 
-  if Rails.env == 'production'
-    db = URI.parse(ENV['MONGOHQ_URL'])
-    db_name = db.path.gsub(/^\//, '')
-    connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-    connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-    config.client = connection
-  else
-    config.client = Mongo::Connection.new
+  unless ENV['MONGO_URI']
+    config.mongo_config_file = Rails.root.join("config/mongo.yml")
   end
+
+  #   db = URI.parse(ENV['MONGOHQ_URL'])
+  #   db_name = db.path.gsub(/^\//, '')
+  #   connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  #   connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  #   config.client = connection
 
 end
